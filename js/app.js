4af15a5319e6452dbbb686adb0ba59f57a4bbbef
Michael Whelan <mjmwdev@gmail.com>
2014-05-16 15:23:34 +0100

    Created dynamic currency converter.

---
+++ js/app.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Michael on 16/05/2014.
+ */
+
+var rates;
+var currencies;
+
+$(document).ready(function() {
+    buildCombos();
+    initialize();
+
+    $("#convertForm").submit(function(event){
+        event.preventDefault();
+
+
+        app_id="c3d8f8a350fb424e9571714ffdfab367";
+        uri=encodeURI("http://openexchangerates.org/latest.json?app_id="+app_id);
+
+        $.get(uri,function(json) {
+// the returned data is available in the json object within the scope of this function
+// rates are available in json.rates as properties with their 3 letter international currency short-codes
+// Eg. get the value of 1 EUR to base currency (by default USD) :
+            var amount = parseFloat($("#inputAmount").val()).toFixed(2);
+            var from = $("#inputFromCurrency").val();
+            var to = $("#inputToCurrency").val();
+            var conversion = (amount/json.rates[from]) * json.rates[to];
+            conversion = parseFloat(conversion).toFixed(2);
+
+            var result = amount + " " + from + " = " + conversion + " " + to;
+            $("#conversion").text(result);
+        },"jsonp");
+    });
+
+});
+
+function initialize() {
+    $("#inputAmount").val(1000);
+    $("#inputFromCurrency").val("GBP");
+    $("#inputToCurrency").val("NZD");
+}
+
+function buildCombos()
+{
+    $.getJSON ("http://openexchangerates.org/api/currencies.json",
+        function(jsondata) {
+            var html = '<option value="">Select a currency</option>';
+            for(var prop in jsondata) {
+                var name = prop + ": " + jsondata[prop];
+                html += '<option value="' + prop +'">' + name + '</option>';
+            }
+            $("#inputFromCurrency").html(html);
+            $("#inputToCurrency").html(html);
+        }
+    );
+
+};
